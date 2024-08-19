782b7b3a3c3ec705fdc04e1bc943e7686ab31e35
SaarAttarchi <saar.attarchi@gmail.com>
2024-08-19 18:18:41 +0300

    change

---
+++ app/src/main/java/com/example/watch_master/interfaces/EpisodeUpdateCallback.java
@@ -0,0 +1,9 @@
+package com.example.watch_master.interfaces;
+
+import com.example.watch_master.models.Episode;
+
+import java.util.HashMap;
+
+public interface EpisodeUpdateCallback {
+    void onEpisodeUpdate();
+}
